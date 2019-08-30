c714ddeaf46ec148bd06f661466edae2af871fc9
aravinth <tech@streamhash.com>
2019-08-30 15:56:22 +0530

    PPV is completed

--- src/components/User/Settings/Invoice.js
+++ src/components/User/Settings/Invoice.js
@@ -180,16 +180,9 @@ class InvoiceComponent extends Helper {
 
       let env = "sandbox"; // you can set here to 'production' for production
       let currency = "USD"; // or you can set this value from your props or state
-      let total = 1; // same as above, this is the total amount (based on currency) to be paid by using Paypal express checkout
-      // Document on Paypal's currency code: https://developer.paypal.com/docs/classic/api/currency_codes/
-
-      // In order to get production's app-ID, you will have to send your app to Paypal for approval first
-      // For sandbox app-ID (after logging into your developer account, please locate the "REST API apps" section, click "Create App"):
-      //   => https://developer.paypal.com/docs/classic/lifecycle/sb_credentials/
-      // For production app-ID:
-      //   => https://developer.paypal.com/docs/classic/lifecycle/goingLive/
-
-      // NB. You can also have many Paypal express checkout buttons on page, just pass in the correct amount and they will work!
+      let total = loadingPromoCode
+        ? subscription.amount
+        : promoCode.remaining_amount; // same as above, this is the total amount (based on currency) to be paid by using Paypal express checkout
 
       const client = {
         sandbox:
@@ -197,6 +190,7 @@ class InvoiceComponent extends Helper {
         production:
           "AUr5bpSyosn6V5uIdnUdqvCWLR6DXgz0bjBBOLKUUjHQFWVfjudQCCzAjpVaBNHeQ_gn575mJCVoYFBo"
       };
+
       return (
         <div>
           <div className="main">
--- src/components/User/Settings/PayPerView.js
+++ src/components/User/Settings/PayPerView.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
-
-// import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 import ContentLoader from "../../Static/contentLoader";
+import PaypalExpressBtn from "react-paypal-express-checkout";
+import { withToastManager } from "react-toast-notifications";
+import ToastDemo from "../../Helper/toaster";
+import api from "../../../Environment";
+import Helper from "../../Helper/helper";
 
-class PayPerView extends Component {
+class PayPerView extends Helper {
   state = {
-    loadingFirst: true
+    loadingFirst: true,
+    data: {},
+    promoCode: null,
+    loadingPromoCode: true,
+    paymentMode: "card",
+    loadingContent: null,
+    buttonDisable: false,
+    loadingContentCard: null,
+    buttonDisableCard: false
   };
   componentDidMount() {
     if (this.props.location.state) {
@@ -14,6 +26,109 @@ class PayPerView extends Component {
       window.location = "/home";
     }
   }
+
+  handlePromoCode = event => {
+    event.preventDefault();
+    this.setState({
+      loadingContent: "Loading... Please wait..",
+      buttonDisable: true
+    });
+    let inputData = {
+      coupon_code: this.state.data.coupon_code,
+      admin_video_id: this.props.location.state.videoDetailsFirst.admin_video_id
+    };
+    api
+      .postMethod("apply/coupon/ppv", inputData)
+      .then(response => {
+        if (response.data.success) {
+          ToastDemo(
+            this.props.toastManager,
+            "Promo code applied successfully!",
+            "success"
+          );
+          this.setState({
+            loadingContent: null,
+            buttonDisable: false,
+            loadingPromoCode: false,
+            promoCode: response.data.data
+          });
+          this.setState({ loadingContent: null, buttonDisable: false });
+        } else {
+          ToastDemo(
+            this.props.toastManager,
+            response.data.error_messages,
+            "error"
+          );
+          this.setState({ loadingContent: null, buttonDisable: false });
+        }
+      })
+      .catch(error => {
+        ToastDemo(this.props.toastManager, error, "error");
+        this.setState({ loadingContent: null, buttonDisable: false });
+      });
+  };
+
+  handlePromoCodeCancel = event => {
+    event.preventDefault();
+    this.setState({ promoCode: null, loadingPromoCode: true });
+    ToastDemo(this.props.toastManager, "Promo code removed..", "error");
+  };
+
+  handleChangePayment = ({ currentTarget: input }) => {
+    this.setState({ paymentMode: input.value });
+  };
+  handlePayment = event => {
+    event.preventDefault();
+    this.setState({
+      loadingContentCard: "Loading... Please wait..",
+      buttonDisableCard: true
+    });
+    let inputData;
+    if (this.state.promoCode == null) {
+      inputData = {
+        admin_video_id: this.props.location.state.videoDetailsFirst
+          .admin_video_id,
+        payment_mode: this.state.paymentMode
+      };
+    } else {
+      inputData = {
+        coupon_code: this.state.data.coupon_code,
+
+        admin_video_id: this.props.location.state.videoDetailsFirst
+          .admin_video_id,
+        payment_mode: this.state.paymentMode
+      };
+    }
+    this.paymentApiCall(inputData);
+  };
+
+  paymentApiCall = inputData => {
+    api
+      .postMethod("v4/ppv_payment", inputData)
+      .then(response => {
+        if (response.data.success) {
+          ToastDemo(this.props.toastManager, response.data.message, "success");
+          this.setState({
+            loadingContentCard: null,
+            buttonDisableCard: false
+          });
+          this.setState({ loadingContentCard: null, buttonDisableCard: false });
+          this.props.history.push("/billing-details");
+        } else {
+          ToastDemo(
+            this.props.toastManager,
+            response.data.error_messages,
+            "error"
+          );
+          this.setState({ loadingContentCard: null, buttonDisableCard: false });
+        }
+      })
+      .catch(error => {
+        ToastDemo(this.props.toastManager, error, "error");
+        this.setState({ loadingContentCard: null, buttonDisableCard: false });
+      });
+  };
+
   render() {
     var billingImg = {
       backgroundImage: "url(assets/img/billing.jpg)"
@@ -22,6 +137,57 @@ class PayPerView extends Component {
       return <ContentLoader />;
     } else {
       const { videoDetailsFirst } = this.props.location.state;
+      const { data, loadingPromoCode, promoCode, paymentMode } = this.state;
+
+      const onSuccess = payment => {
+        // Congratulation, it came here means everything's fine!
+
+        let inputData;
+        if (this.state.promoCode == null) {
+          inputData = {
+            admin_video_id: this.props.location.state.videoDetailsFirst
+              .admin_video_id,
+            payment_mode: this.state.paymentMode,
+            payment_id: payment.paymentID
+          };
+        } else {
+          inputData = {
+            coupon_code: this.state.data.coupon_code,
+
+            admin_video_id: this.props.location.state.videoDetailsFirst
+              .admin_video_id,
+            payment_mode: this.state.paymentMode,
+            payment_id: payment.paymentID
+          };
+        }
+        this.paymentApiCall(inputData);
+
+        // You can bind the "payment" object's value to your state or props or whatever here, please see below for sample returned data
+      };
+
+      const onCancel = data => {
+        // User pressed "cancel" or close Paypal's popup!
+        // You can bind the "data" object's value to your state or props or whatever here, please see below for sample returned data
+      };
+
+      const onError = err => {
+        // The main Paypal's script cannot be loaded or somethings block the loading of that script!
+        // Because the Paypal's main script is loaded asynchronously from "https://www.paypalobjects.com/api/checkout.js"
+        // => sometimes it may take about 0.5 second for everything to get set, or for the button to appear
+      };
+
+      let env = "sandbox"; // you can set here to 'production' for production
+      let currency = "USD"; // or you can set this value from your props or state
+      let total = loadingPromoCode
+        ? videoDetailsFirst.ppv_amount
+        : promoCode.remaining_amount; // same as above, this is the total amount (based on currency) to be paid by using Paypal express checkout
+
+      const client = {
+        sandbox:
+          "AX5Ut9TBRNu6dgVBTNo0pOzRo7APIhfoVg34QzfycTZaQOT6vKN1B2Kpej4isx8VNDf26tsdbQAN4QjD",
+        production:
+          "AUr5bpSyosn6V5uIdnUdqvCWLR6DXgz0bjBBOLKUUjHQFWVfjudQCCzAjpVaBNHeQ_gn575mJCVoYFBo"
+      };
 
       return (
         <div>
@@ -142,14 +308,21 @@ class PayPerView extends Component {
                                 </td>
                               </tr>
                               <tr>
-                                <td>tax</td>
-                                <td>$5.00</td>
+                                <td>Promo Code amount</td>
+                                <td>
+                                  {videoDetailsFirst.currency}
+                                  {loadingPromoCode
+                                    ? "0"
+                                    : promoCode.coupon_amount}
+                                </td>
                               </tr>
                               <tr className="table-secondary">
                                 <td>total</td>
                                 <td>
                                   {videoDetailsFirst.currency}
-                                  {videoDetailsFirst.ppv_amount}
+                                  {loadingPromoCode
+                                    ? videoDetailsFirst.ppv_amount
+                                    : promoCode.remaining_amount}
                                 </td>
                               </tr>
                             </tbody>
@@ -160,22 +333,48 @@ class PayPerView extends Component {
                         {/* <!-- coupon --> */}
                         <div className="mt-4">
                           <h5 className="capitalize">have a coupon?</h5>
-                          <form className="auth-form">
+                          <form
+                            className="auth-form"
+                            onSubmit={this.handlePromoCode}
+                          >
                             <div className="form-group mt-3">
                               <div className="input-group mb-3 mt-1">
                                 <input
                                   type="text"
                                   className="form-control m-0 mb-0"
                                   placeholder="promo code"
+                                  name="coupon_code"
+                                  value={data.coupon_code}
+                                  onChange={this.handleChange}
                                 />
                                 <div className="input-group-append">
-                                  <button className="btn btn-danger">
-                                    send
+                                  <button
+                                    className="btn btn-danger"
+                                    type="submit"
+                                  >
+                                    {this.state.loadingContent != null
+                                      ? this.state.loadingContent
+                                      : "send"}
                                   </button>
                                 </div>
                               </div>
                             </div>
                           </form>
+                          {loadingPromoCode ? (
+                            ""
+                          ) : (
+                            <p className="capitalize">
+                              Promo code applied - {promoCode.coupon_code} for{" "}
+                              {promoCode.original_coupon_amount} -{" "}
+                              <Link
+                                to="#"
+                                className="btn-danger"
+                                onClick={this.handlePromoCodeCancel}
+                              >
+                                Cancel
+                              </Link>
+                            </p>
+                          )}
                         </div>
                         {/* <!-- coupon --> */}
 
@@ -186,24 +385,52 @@ class PayPerView extends Component {
                             <div className="form-check-inline">
                               <input
                                 type="radio"
-                                id="test1"
-                                name="radio-group"
-                                checked
+                                id="paypal"
+                                name="payment_mode"
+                                value="paypal"
+                                onChange={this.handleChangePayment}
                               />
-                              <label for="test1">paypal</label>
+                              <label htmlFor="paypal">paypal</label>
                             </div>
                             <div className="form-check-inline">
                               <input
                                 type="radio"
-                                id="test2"
-                                name="radio-group"
+                                id="card"
+                                name="payment_mode"
+                                defaultChecked={true}
+                                value="card"
+                                onChange={this.handleChangePayment}
                               />
-                              <label for="test2">card payment</label>
+                              <label htmlFor="card">card payment</label>
                             </div>
-                            <div className="text-right mb-3">
-                              <button className="btn btn-danger">
-                                pay now
-                              </button>
+                            <Link
+                              to="/add-card"
+                              className="float-right btn-link"
+                            >
+                              add card
+                            </Link>
+                            <div className="text-right mb-3 mt-3">
+                              {paymentMode == "card" ? (
+                                <button
+                                  className="btn btn-danger"
+                                  onClick={this.handlePayment}
+                                  disabled={this.state.buttonDisableCard}
+                                >
+                                  {this.state.loadingContentCard != null
+                                    ? this.state.loadingContentCard
+                                    : "pay now using Card"}
+                                </button>
+                              ) : (
+                                <PaypalExpressBtn
+                                  env={env}
+                                  client={client}
+                                  currency={currency}
+                                  total={total}
+                                  onError={onError}
+                                  onSuccess={onSuccess}
+                                  onCancel={onCancel}
+                                />
+                              )}
                             </div>
                           </form>
                         </div>
@@ -221,4 +448,4 @@ class PayPerView extends Component {
   }
 }
 
-export default PayPerView;
+export default withToastManager(PayPerView);
