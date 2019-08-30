17483a7687390bc5cb5fdaafb921e7f2d59dc2be
aravinth <tech@streamhash.com>
2019-08-30 22:14:37 +0530

    cast and crew issue fixed

--- src/components/User/viewAll.jsx
+++ src/components/User/viewAll.jsx
@@ -17,11 +17,13 @@ class ViewAll extends Component {
       window.location = "/home";
     }
     let inputData;
+    let apiURL;
     if (this.props.location.state.videoType != undefined) {
       inputData = {
         skip: 0,
         cast_crew_id: this.props.location.state.cast_crew_id
       };
+      apiURL = "v4/cast_crews/videos";
     } else {
       inputData = {
         skip: 0,
@@ -29,18 +31,21 @@ class ViewAll extends Component {
         page_type: this.props.location.state.page_type,
         category_id: this.props.location.state.category_id
       };
+      apiURL = "see_all";
     }
-    this.viewAllApiCall(inputData);
+    this.viewAllApiCall(inputData, apiURL);
   }
 
   //WARNING! To be deprecated in React v17. Use new lifecycle static getDerivedStateFromProps instead.
   componentWillReceiveProps(nextProps) {
     let inputData;
+    let apiURL;
     if (nextProps.location.state.videoType != undefined) {
       inputData = {
         skip: 0,
         cast_crew_id: nextProps.location.state.cast_crew_id
       };
+      apiURL = "v4/cast_crews/videos";
     } else {
       inputData = {
         skip: 0,
@@ -48,13 +53,14 @@ class ViewAll extends Component {
         page_type: nextProps.location.state.page_type,
         category_id: nextProps.location.state.category_id
       };
+      apiURL = "see_all";
     }
-    this.viewAllApiCall(inputData);
+    this.viewAllApiCall(inputData, apiURL);
   }
 
-  viewAllApiCall = inputData => {
+  viewAllApiCall = (inputData, apiURL) => {
     api
-      .postMethod("see_all", inputData)
+      .postMethod(apiURL, inputData)
       .then(response => {
         if (response.data.success) {
           this.setState({ loading: false, videoList: response.data.data });
