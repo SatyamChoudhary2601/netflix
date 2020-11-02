9c02713809230f84fa569a04f0009f42a5e31491
vithyaCodegama <vithya@codegama.com>
2020-11-02 21:45:51 +0530

    Removd Email from LandingPage

--- src/components/LatestLandingPage/LatestLandingPage.jsx
+++ src/components/LatestLandingPage/LatestLandingPage.jsx
@@ -33,7 +33,7 @@ class LatestLandingPage extends Component {
     });
   }
 
-  async getFaqs () {
+  async getFaqs() {
     api.getMethod("faqs/list").then((response) => {
       if (response.data.success) {
         this.setState({
@@ -45,14 +45,14 @@ class LatestLandingPage extends Component {
     });
   }
   render() {
-    const { loading, HomeSettings,loadingData, faqData } = this.state;
+    const { loading, HomeSettings, loadingData, faqData } = this.state;
 
     if (
       localStorage.getItem("userId") &&
       localStorage.getItem("active_profile_id") &&
       localStorage.getItem("accessToken")
     )
-    return <Redirect to="/home" />;
+      return <Redirect to="/home" />;
     return (
       <>
         <div
@@ -85,7 +85,7 @@ class LatestLandingPage extends Component {
             </div>
             <div className="latest-banner-content-info-form">
               <ul className="list-unstyled banner-theme-form">
-                 <li>
+                {/* <li>
                   <form className="theme-form-sec">
                     <div className="form-group">
                       <input
@@ -95,7 +95,7 @@ class LatestLandingPage extends Component {
                       />
                     </div>
                   </form>
-                </li> 
+                </li>  */}
                 <li>
                   <button className="btn btn-search">
                     <Link to="/register">
@@ -113,9 +113,11 @@ class LatestLandingPage extends Component {
             <div className="row">
               <div className="col-md-6">
                 <div className="about-details">
-                  <h2 className="about-title">{HomeSettings.home_section_1_title}</h2>
+                  <h2 className="about-title">
+                    {HomeSettings.home_section_1_title}
+                  </h2>
                   <h4 className="about-desc">
-                  {HomeSettings.home_section_1_description}
+                    {HomeSettings.home_section_1_description}
                   </h4>
                 </div>
               </div>
@@ -149,16 +151,19 @@ class LatestLandingPage extends Component {
               <div className="col-md-6 d-block d-sm-none">
                 <div className="download-details">
                   <h2 className="download-title">
-                  {HomeSettings.home_section_2_title}
+                    {HomeSettings.home_section_2_title}
                   </h2>
                   <h4 className="download-desc">
-                  {HomeSettings.home_section_2_description}
+                    {HomeSettings.home_section_2_description}
                   </h4>
                 </div>
               </div>
               <div className="col-md-6">
                 <div className="mobile-img-sec">
-                  <img src={HomeSettings.home_section_2_image} className="mobile-img" />
+                  <img
+                    src={HomeSettings.home_section_2_image}
+                    className="mobile-img"
+                  />
                 </div>
                 <div className="our-download-card">
                   <div className="our-download-info">
@@ -170,7 +175,9 @@ class LatestLandingPage extends Component {
                       />
                     </div>
                     <div className="our-download-card-text">
-                      <h4 className="download-sub-title">{HomeSettings.home_section_2_image_title}</h4>
+                      <h4 className="download-sub-title">
+                        {HomeSettings.home_section_2_image_title}
+                      </h4>
                       <p className="download-sub-desc">{t("downloading")}...</p>
                     </div>
                   </div>
@@ -185,10 +192,10 @@ class LatestLandingPage extends Component {
               <div className="col-md-6 d-none d-sm-block">
                 <div className="download-details">
                   <h2 className="download-title">
-                  {HomeSettings.home_section_2_title}
+                    {HomeSettings.home_section_2_title}
                   </h2>
                   <h4 className="download-desc">
-                  {HomeSettings.home_section_2_description}
+                    {HomeSettings.home_section_2_description}
                   </h4>
                 </div>
               </div>
@@ -200,9 +207,11 @@ class LatestLandingPage extends Component {
             <div className="row">
               <div className="col-md-6">
                 <div className="watch-everywhere-details">
-                  <h2 className="watch-everywhere-title">{HomeSettings.home_section_3_title}</h2>
+                  <h2 className="watch-everywhere-title">
+                    {HomeSettings.home_section_3_title}
+                  </h2>
                   <h4 className="watch-everywhere-desc">
-                  {HomeSettings.home_section_3_description}
+                    {HomeSettings.home_section_3_description}
                   </h4>
                 </div>
               </div>
@@ -238,43 +247,42 @@ class LatestLandingPage extends Component {
             <div className="faq-lists-sec">
               <h1 className="section-title">{t("faq_title")}</h1>
               <div className="accordion" id="accordionExample">
+                {loadingData
+                  ? "Loading..."
+                  : faqData.length > 0
+                  ? faqData.map((faq) => (
+                      <div className="card">
+                        <div className="card-header" id={faq.unique_id}>
+                          <h2 className="mb-0">
+                            <button
+                              className="btn btn-link btn-block text-left heading-title collapsed"
+                              type="button"
+                              data-toggle="collapse"
+                              data-target={`#${faq.faq_id}`}
+                              aria-expanded="false"
+                              aria-controls="collapseOne"
+                            >
+                              {faq.question}
+                            </button>
+                          </h2>
+                        </div>
 
-                {loadingData ? ( "Loading..." ) : faqData.length > 0 ? (
-                  faqData.map((faq) => (
-                    <div className="card">
-                      <div className="card-header" id={faq.unique_id}>
-                        <h2 className="mb-0">
-                          <button
-                            className="btn btn-link btn-block text-left heading-title collapsed"
-                            type="button"
-                            data-toggle="collapse"
-                            data-target={`#${faq.faq_id}`}
-                            aria-expanded="false"
-                            aria-controls="collapseOne"
-                          >
-                            {faq.question}
-                          </button>
-                        </h2>
-                      </div>
-
-                      <div
-                        id={faq.faq_id}
-                        className="collapse"
-                        aria-labelledby={faq.unique_id}
-                        data-parent="#accordionExample"
-                      >
-                        <div className="card-body">
-                          {renderHTML(faq.answer)}
+                        <div
+                          id={faq.faq_id}
+                          className="collapse"
+                          aria-labelledby={faq.unique_id}
+                          data-parent="#accordionExample"
+                        >
+                          <div className="card-body">
+                            {renderHTML(faq.answer)}
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  ))
-                ) : ("")}
+                    ))
+                  : ""}
               </div>
               <div className="latest-banner-content-info-form">
-                <p className="faq-desc">
-                {t("ready_to_watch")}
-                </p>
+                <p className="faq-desc">{t("ready_to_watch")}</p>
                 <ul className="list-unstyled banner-theme-form form-align-center">
                   {/* <li>
                     <form className="theme-form-sec">
